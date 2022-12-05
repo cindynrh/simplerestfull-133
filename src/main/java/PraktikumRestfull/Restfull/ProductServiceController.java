4e8f7febe5bdc82e87d01960e2b674ef7673f519
cindynrh <cindynurhabibah@gmail.com>
2022-12-05 16:50:33 +0700

    controller

---
+++ src/main/java/PraktikumRestfull/Restfull/ProductServiceController.java
@@ -0,0 +1,32 @@
+package PraktikumRestfull.Restfull;
+
+import PraktikumRestfull.Restfull.pws.Product;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+@RestController
+public class ProductServiceController {
+    private static Map<String, Product> productRepo = new HashMap<>();
+    static {
+        Product honey = new Product();
+        honey.setId("1");
+        honey.setName("Honey");
+        productRepo.put(honey.getId(), honey);
+        
+        Product almond = new Product();
+        almond.setId("2");
+        almond.setName("Almond");
+        productRepo.put(almond.getId(), almond);
+    }
+}
